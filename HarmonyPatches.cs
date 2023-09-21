8df0bc3619d9bab809688475083d9ded6075367f
Noelle Lavenza <jadebishop.2562@gmail.com>
2023-09-21 15:51:58 -0500

    Fix GetFeeling patch

--- HarmonyPatches.cs
+++ HarmonyPatches.cs
@@ -7,12 +7,12 @@ namespace RitaMod.HarmonyPatches{
 	[HarmonyPatch(typeof(Brain), "GetFeeling", new Type[] { typeof(GameObject) })]
 	public class GetFeeling_Patch
 	{
-		static void Postfix(ref Brain __instance, ref int  __result, GameObject GO)
+		static void Postfix(ref Brain __instance, ref int  __result, GameObject Target)
 		{
 			if (__instance.ParentObject.HasRegisteredEvent("GetFeeling"))
 			{
 				Event eGetFeeling = Event.New("GetFeeling");
-				eGetFeeling.SetParameter("Who", GO);
+				eGetFeeling.SetParameter("Who", Target);
 				eGetFeeling.SetParameter("Feeling", __result);
 				__instance.ParentObject.FireEvent(eGetFeeling);
 				__result = eGetFeeling.GetIntParameter("Feeling");
